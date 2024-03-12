ea204ead5932d7a2626490aec7f5364b8740f888
Tivin <53348536+Tivin-i@users.noreply.github.com>
2024-03-12 15:50:19 +0800

    Update generate-nginx-configs.sh
    Better handling of self-generated certificates.

--- scripts/generate-nginx-configs.sh
+++ scripts/generate-nginx-configs.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # Remove default Nginx configuration
 rm -f /etc/nginx/conf.d/default.conf
+
+# Create a directory for self-signed certificates
+mkdir -p /etc/nginx/ssl
 
 # Iterate over directories in /app/sites/
 for domain_dir in /app/sites/*/; do
@@ -13,15 +16,9 @@ for domain_dir in /app/sites/*/; do
 
         # Obtain SSL certificate using Certbot with RFC-2136
         certbot certonly --non-interactive --agree-tos --email "${EMAIL}" --dns-rfc2136 --dns-rfc2136-credentials /app/config/certbot.ini --deploy-hook "/app/scripts/renew-hook.sh" -d ${domain} -d www.${domain}
-    else
-        echo "Dry run failed for ${domain}. Generating self-signed certificate."
 
-        # Create a self-signed certificate for the domain
-        openssl req -x509 -nodes -newkey rsa:2048 -days 365 -keyout /etc/letsencrypt/live/${domain}/fullchain.pem -out /etc/letsencrypt/live/${domain}/privkey.pem -subj "/CN=${domain}"
-    fi
-
-    # Generate Nginx configuration file for the domain
-    cat > "/etc/nginx/conf.d/${domain}.conf" <<EOL
+        # Generate Nginx configuration file for the domain with Let's Encrypt certificates
+        cat > "/etc/nginx/conf.d/${domain}.conf" <<EOL
 server {
     listen 80;
     server_name ${domain} www.${domain};
@@ -44,5 +41,35 @@ server {
     }
 }
 EOL
+    else
+        echo "Dry run failed for ${domain}. Creating self-signed certificate."
 
+        # Create a self-signed certificate for the domain
+        openssl req -x509 -nodes -newkey rsa:2048 -days 365 -keyout /etc/nginx/ssl/${domain}.key -out /etc/nginx/ssl/${domain}.crt -subj "/CN=${domain}"
+
+        # Generate Nginx configuration file for the domain with self-signed certificates
+        cat > "/etc/nginx/conf.d/${domain}.conf" <<EOL
+server {
+    listen 80;
+    server_name ${domain} www.${domain};
+
+    location / {
+        return 301 https://\$host\$request_uri;
+    }
+}
+
+server {
+    listen 443 ssl;
+    server_name ${domain} www.${domain};
+
+    ssl_certificate /etc/nginx/ssl/${domain}.crt;
+    ssl_certificate_key /etc/nginx/ssl/${domain}.key;
+
+    location / {
+        root /app/sites/${domain};
+        index index.html;
+    }
+}
+EOL
+    fi
 done
